add8e4aa087c03c307b49f785dc275f5040a32b3
Nicolas <nicolasgp.ec@gmail.com>
2024-12-13 16:35:24 -0300

    feat(config): update image configuration to use remote patterns for image domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: ["images.unsplash.com", "picsum.photos"]
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+				port: "",
+				pathname: "**/*"
+			},
+			{
+				protocol: "https",
+				hostname: "picsum.photos",
+				port: "",
+				pathname: "**/*"
+			}
+		]
 	}
 };
 
