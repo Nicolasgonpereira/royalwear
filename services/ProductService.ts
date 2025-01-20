0d55b8081cdf974a51e8cf353198309e2e409065
Nicolas <nicolasgp.ec@gmail.com>
2025-01-20 16:08:01 -0300

    feat(product): update product service to use IProduct interface and enhance getProductById response structure

--- services/ProductService.ts
+++ services/ProductService.ts
@@ -1,8 +1,9 @@
+import { IProduct } from "@/app/products/[id]/[[...productName]]/page";
 import { Product } from "@prisma/client";
 
 const apiUrl: string = process.env.API_URL || "http://localhost:3000/api";
 
-export async function getProducts(): Promise<Product[] | undefined> {
+export async function getProducts(): Promise<IProduct[] | undefined> {
 	try {
 		const response = await fetch(`${apiUrl}/products`);
 		if (!response.ok) {
@@ -15,9 +16,25 @@ export async function getProducts(): Promise<Product[] | undefined> {
 	}
 }
 
+interface IProductbyId extends Product {
+	category: {
+		id: string;
+		name: string;
+		type: string;
+	};
+	reviews: {
+		id: string;
+		userId: string;
+		productId: string;
+		rating: number;
+		createAt: Date;
+		comment?: string;
+	}[];
+}
+
 export async function getProductById(
 	productId: number
-): Promise<Product | undefined> {
+): Promise<IProductbyId | undefined> {
 	try {
 		const response = await fetch(`${apiUrl}/products/${productId}`);
 		if (!response.ok) {
@@ -30,7 +47,7 @@ export async function getProductById(
 	}
 }
 
-export async function getFeaturedProducts(): Promise<Product[] | undefined> {
+export async function getFeaturedProducts(): Promise<IProduct[] | undefined> {
 	try {
 		const response = await fetch(`${apiUrl}/products`);
 		if (!response.ok) {
